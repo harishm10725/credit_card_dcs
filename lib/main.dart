bcc87b20746dface23a0d734f1e127ac72ddd678
saivarshith67 <saivarshith.coder@gmail.com>
2024-03-26 21:08:20 +0530

    Change to CreditCardForm

--- lib/main.dart
+++ lib/main.dart
@@ -3,174 +3,133 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
 import 'package:icons_flutter/icons_flutter.dart';
-import 'package:flutter/services.dart';
-import 'separator.dart';
 void main() {
-  runApp(mainapp());
+  runApp(MainApp());
 }
 
-class mainapp extends StatelessWidget {
-  const mainapp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return creditcardscreen();
+    return MaterialApp(
+      home: CreditCardScreen(),
+    );
   }
 }
 
-class creditcardscreen extends StatefulWidget {
-  const creditcardscreen({super.key});
+class CreditCardScreen extends StatefulWidget {
+  const CreditCardScreen({super.key});
 
   @override
-  State<creditcardscreen> createState() => _creditcardscreenState();
+  State<CreditCardScreen> createState() => _CreditCardScreenState();
 }
 
-class _creditcardscreenState extends State<creditcardscreen> {
+class _CreditCardScreenState extends State<CreditCardScreen> {
   bool isCvvFocused = false;
+  bool showBackView = false;
   String _cardNumber = '';
   String _expiryDate = '';
   String _cardHolderName = '';
   String _cvvCode = '';
+  final formKey = GlobalKey<FormState>();
+
+  void onCreditCardModelChange(CreditCardModel  creditCardModel){
+    setState(() {
+      _cardNumber = creditCardModel.cardNumber;
+      _cvvCode = creditCardModel.cvvCode;
+      _cardHolderName = creditCardModel.cardHolderName;
+      _expiryDate = creditCardModel.expiryDate;
+      isCvvFocused = creditCardModel.isCvvFocused;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: Center(
+        body: SafeArea(
           child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(AntDesign.antdesign,size: 60,),
-                    CreditCardWidget(
-                      cardNumber: _cardNumber,
-                      expiryDate: _expiryDate,
-                      cardHolderName: _cardHolderName,
-                      cvvCode: _cvvCode,
-                      showBackView: false,
-                      //true when you want to show cvv(back) view
-                      onCreditCardWidgetChange: (CreditCardBrand
-                          brand) {}, // Callback for anytime credit card brand is changed
-                    ),
-                    //account number
-                    Padding(
-                      padding: const EdgeInsets.only(left: 13,right: 13,top: 20,bottom: 20),
-                      child: TextField(
-                        onChanged: (value)
-                        {
-                          setState(() {
-                            _cardNumber = value;
-                          });
-                        },
-                        inputFormatters: [
-                          FilteringTextInputFormatter.digitsOnly,
-                          CardNumberFormatter(),
-                        ],
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            labelText: "Card Number",
-                            hintText: "XXXX XXXX XXXX XXXX",
-                            contentPadding: EdgeInsetsDirectional.only(
-                                top: 20.0, bottom: 20.0, start: 25.0, end: 5.0),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                                borderSide: BorderSide(color: Colors.red))),
-                        autofocus: true,
-                        autocorrect: true,
-                      ),
-                    ),
-
-                    Padding(
-
-                      padding: const EdgeInsets.only(left: 13,right: 13,bottom: 20),
-                      child: TextField(
-                        onChanged: (value)
-                        {
-                          setState(() {
-                            _cardHolderName = value;
-                          });
-                         },
-                        decoration: InputDecoration(
-                            labelText: "CardHolder Name",
-                            contentPadding: EdgeInsetsDirectional.only(
-                                top: 20.0, bottom: 20.0, start: 25.0, end: 5.0),
-                            border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                                borderSide: BorderSide(color: Colors.red))),
-                        autofocus: true,
-                        autocorrect: true,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 13, right: 10, top: 5, bottom: 10),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              onChanged: (value)
-                              {
-                                setState(() {
-                                  _expiryDate = value;
-                                });
-                              },
-                              decoration: InputDecoration(
-                                  labelText: "Expiry Date",
-                                  contentPadding: EdgeInsetsDirectional.only(
-                                      top: 20.0,
-                                      bottom: 20.0,
-                                      start: 25.0,
-                                      end: 5.0),
-                                  border: OutlineInputBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(10)))),
-                              autofocus: true,
-                              autocorrect: true,
-                            ),
-                          ),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          Expanded(
-                            child: TextField(
-                              onChanged: (value)
-                              {
-                                setState(() {
-                                  _cvvCode = value;
-                                });
-                              },
-                              decoration: InputDecoration(
-                                  labelText: "Cvv",
-                                  contentPadding: EdgeInsetsDirectional.only(
-                                      top: 20.0,
-                                      bottom: 20.0,
-                                      start: 25.0,
-                                      end: 5.0),
-                                  border: OutlineInputBorder(
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(10)))),
-                              autofocus: true,
-                              autocorrect: true,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20,),
-                    ElevatedButton(
-                      child: const Text('Proceed to Pay'),
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-
-                      ),
-                    ),
-                  ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(AntDesign.antdesign,size: 60,),
+                CreditCardWidget(
+                  cardNumber: _cardNumber,
+                  expiryDate: _expiryDate,
+                  cardHolderName: _cardHolderName,
+                  cvvCode: _cvvCode,
+                  showBackView: showBackView,
+                  onCreditCardWidgetChange: (CreditCardBrand brand) {},
+                  bankName: 'Axis Bank',
+                  obscureCardCvv: true,
+                  obscureCardNumber: true,
+                  isHolderNameVisible: true,
+                  isSwipeGestureEnabled: true,
+                  cardType: CardType.visa,
                 ),
-              ),
+                CreditCardForm(
+                  formKey: formKey,
+                  cardNumber: _cardNumber,
+                  expiryDate: _expiryDate,
+                  cardHolderName: _cardHolderName,
+                  cvvCode: _cvvCode,
+                  obscureCvv: true,
+                  obscureNumber: true,
+                  isCardNumberVisible: true,
+                  isExpiryDateVisible: true,
+                  isHolderNameVisible: true,
+                  onCreditCardModelChange: onCreditCardModelChange,
+                  inputConfiguration: const InputConfiguration(
+                    cardNumberDecoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Number',
+                      hintText: 'XXXX XXXX XXXX XXXX',
+                    ),
+                    expiryDateDecoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Expired Date',
+                      hintText: 'XX/XX',
+                    ),
+                    cvvCodeDecoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'CVV',
+                      hintText: 'XXX',
+                    ),
+                    cardHolderDecoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Card Holder',
+                    ),
+                    cardNumberTextStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                    cardHolderTextStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                    expiryDateTextStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                    cvvCodeTextStyle: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black,
+                    ),
+                  ),
+
+                ),
+                SizedBox(height: 20,),
+
+                ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Complete Payment")
+                ),
+              ],
             ),
+          ),
         ),
         ),
       );
