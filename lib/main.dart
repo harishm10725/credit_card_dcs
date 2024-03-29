8a6e4983938e9fc35b7be225f5eb959d0b06ea76
saivarshith67 <saivarshith.coder@gmail.com>
2024-03-29 22:35:25 +0530

    Add Comments to the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,51 @@
-import 'package:flutter/cupertino.dart';
+// Importing necessary packages
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
-import 'package:icons_flutter/icons_flutter.dart';
+
+// The main function of the application.
 void main() {
-  runApp(MainApp());
+  // Running the MainApp widget
+  runApp(const MainApp());
 }
 
+// Defining a stateless widget MainApp
 class MainApp extends StatelessWidget {
+  // Constructor for MainApp
   const MainApp({super.key});
 
+  // Overriding the build method
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: CreditCardScreen(),
+    // Returning MaterialApp as the root of the widget tree
+    return const MaterialApp(
+      home: CreditCardScreen(), // Setting CreditCardScreen as the home screen
     );
   }
 }
 
+// Defining a stateful widget CreditCardScreen
 class CreditCardScreen extends StatefulWidget {
+  // Constructor for CreditCardScreen
   const CreditCardScreen({super.key});
 
+  // Overriding the createState method to create state for this widget
   @override
   State<CreditCardScreen> createState() => _CreditCardScreenState();
 }
 
+// This line defines a private subclass named _CreditCardScreenState that extends the State class
 class _CreditCardScreenState extends State<CreditCardScreen> {
-  bool isCvvFocused = false;
-  bool showBackView = false;
-  String _cardNumber = '';
-  String _expiryDate = '';
-  String _cardHolderName = '';
-  String _cvvCode = '';
-  final formKey = GlobalKey<FormState>();
+  // These variables store the state of the CreditCardScreen:
+  bool isCvvFocused = false; // Tracks if the CVV field is currently in focus
+  bool showBackView = false;  // Controls whether to display the front or back of the card on the CreditCardWidget
+  String _cardNumber = '';    // Stores the entered credit card number
+  String _expiryDate = '';    // Stores the entered expiry date
+  String _cardHolderName = ''; // Stores the card holder name entered by the user
+  String _cvvCode = '';       // Stores the entered CVV code
+  final formKey = GlobalKey<FormState>(); // A unique key to access the CreditCardForm state
 
-  void onCreditCardModelChange(CreditCardModel  creditCardModel){
+  // This function updates the state variables based on changes in the CreditCardModel object received from the CreditCardForm
+  void onCreditCardModelChange(CreditCardModel creditCardModel) {
     setState(() {
       _cardNumber = creditCardModel.cardNumber;
       _cvvCode = creditCardModel.cvvCode;
@@ -47,91 +58,133 @@ class _CreditCardScreenState extends State<CreditCardScreen> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // Disabling the debug banner
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(
+          // Setting the title of the AppBar
+          title: const Text("Card Settings"),
+          // Centering the title
+          centerTitle: true,
+        ),
+        // Setting the background color of the Scaffold
         backgroundColor: Colors.white,
         body: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Icon(AntDesign.antdesign,size: 60,),
-                CreditCardWidget(
-                  cardNumber: _cardNumber,
-                  expiryDate: _expiryDate,
-                  cardHolderName: _cardHolderName,
-                  cvvCode: _cvvCode,
-                  showBackView: showBackView,
-                  onCreditCardWidgetChange: (CreditCardBrand brand) {},
-                  bankName: 'Axis Bank',
-                  obscureCardCvv: true,
-                  obscureCardNumber: true,
-                  isHolderNameVisible: true,
-                  isSwipeGestureEnabled: true,
-                  cardType: CardType.visa,
-                ),
-                CreditCardForm(
-                  formKey: formKey,
-                  cardNumber: _cardNumber,
-                  expiryDate: _expiryDate,
-                  cardHolderName: _cardHolderName,
-                  cvvCode: _cvvCode,
-                  obscureCvv: true,
-                  obscureNumber: true,
-                  isCardNumberVisible: true,
-                  isExpiryDateVisible: true,
-                  isHolderNameVisible: true,
-                  onCreditCardModelChange: onCreditCardModelChange,
-                  inputConfiguration: const InputConfiguration(
-                    cardNumberDecoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Number',
-                      hintText: 'XXXX XXXX XXXX XXXX',
-                    ),
-                    expiryDateDecoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Expired Date',
-                      hintText: 'XX/XX',
-                    ),
-                    cvvCodeDecoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'CVV',
-                      hintText: 'XXX',
-                    ),
-                    cardHolderDecoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Card Holder',
-                    ),
-                    cardNumberTextStyle: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                    cardHolderTextStyle: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                    expiryDateTextStyle: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
-                    cvvCodeTextStyle: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Creating a CreditCardWidget
+                  CreditCardWidget(
+                    // Setting the card number
+                    cardNumber: _cardNumber,
+                    // Setting the expiry date
+                    expiryDate: _expiryDate,
+                    // Setting the card holder name
+                    cardHolderName: _cardHolderName,
+                    // Setting the CVV code
+                    cvvCode: _cvvCode,
+                    // Deciding whether to show the back view
+                    showBackView: showBackView,
+                    // Setting the callback for when the credit card widget changes
+                    onCreditCardWidgetChange: (CreditCardBrand brand) {},
+                    // Obscuring the CVV
+                    obscureCardCvv: true,
+                    // Obscuring the card number
+                    obscureCardNumber: true,
+                    // Deciding whether to show the holder name
+                    isHolderNameVisible: true,
+                    // Enabling swipe gestures
+                    isSwipeGestureEnabled: true,
+                    // Setting the card type
+                    cardType: CardType.mastercard,
+                  ),
+                  // Creating a CreditCardForm
+                  CreditCardForm(
+                    // Setting the form key
+                    formKey: formKey,
+                    // Setting the card number
+                    cardNumber: _cardNumber,
+                    // Setting the expiry date
+                    expiryDate: _expiryDate,
+                    // Setting the card holder name
+                    cardHolderName: _cardHolderName,
+                    // Setting the CVV code
+                    cvvCode: _cvvCode,
+                    // Obscuring the CVV
+                    obscureCvv: true,
+                    // Obscuring the card number
+                    obscureNumber: true,
+                    // Deciding whether to show the card number
+                    isCardNumberVisible: true,
+                    // Deciding whether to show the expiry date
+                    isExpiryDateVisible: true,
+                    // Deciding whether to show the holder name
+                    isHolderNameVisible: true,
+                    // Setting the callback for when the credit card model changes
+                    onCreditCardModelChange: onCreditCardModelChange,
+                    // Setting the input configuration
+                    inputConfiguration: const InputConfiguration(
+                      // Setting the decoration for the card number input field
+                      cardNumberDecoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Number',
+                        hintText: 'XXXX XXXX XXXX XXXX',
+                      ),
+                      // Setting the decoration for the expiry date input field
+                      expiryDateDecoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Expired Date',
+                        hintText: 'XX/XX',
+                      ),
+                      // Setting the decoration for the CVV input field
+                      cvvCodeDecoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'CVV',
+                        hintText: 'XXX',
+                      ),
+                      // Setting the decoration for the card holder input field
+                      cardHolderDecoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Card Holder',
+                      ),
+                      // Setting the text style for the card number
+                      cardNumberTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      // Setting the text style for the card holder
+                      cardHolderTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      // Setting the text style for the expiry date
+                      expiryDateTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      // Setting the text style for the CVV
+                      cvvCodeTextStyle: TextStyle(
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
                     ),
                   ),
-
-                ),
-                SizedBox(height: 20,),
-
-                ElevatedButton(
+                  // Adding some space
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  // Creating a checkout button
+                  ElevatedButton(
+                    child: const Text('Checkout'),
                     onPressed: () {},
-                    child: Text("Complete Payment")
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-        ),
-      );
+      ),
+    );
   }
 }
